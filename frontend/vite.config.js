dcd7006fe18fc286f3cbc0dc11ef5c2b727c5c1e
Albert <albdanvic@alu.edu.gva>
2026-03-29 21:01:59 +0200

    fix(nginx): correct internal backend proxy port and add phpmyadmin setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         },
         proxy: {
             '/api': {
-                target: 'http://app',
+                target: 'http://localhost:8000',
                 changeOrigin: true,
                 secure: false,
             },
