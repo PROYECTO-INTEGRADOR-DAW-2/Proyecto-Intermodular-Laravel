b55fd077c595d3b48471fe033a2083020262a77c
Albert <albdanvic@alu.edu.gva>
2026-03-30 22:25:21 +0200

    Fix: Enviar ID de usuario en la respuesta de la API para permitir edición de reviews

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ Route::get('oauth/google/callback', [SocialAuthController::class , 'callback']);
 Route::get('/user', function (Request $request) {
     $user = $request->user()->load('roles');
     return response()->json([
+        'id'    => $user->id,
         'name'  => $user->nombre,
         'email' => $user->email,
         'role'  => $user->role,
